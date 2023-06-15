13a9b31963863ef597f5550b50e85a94c6d9bb65
Nicolo Castelnuovo <ncasteln@2-B-3.42heilbronn.de>
2023-06-15 09:23:27 +0200

    Ready to test clst

--- Makefile
+++ Makefile
@@ -6,17 +6,24 @@
 #    By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/27 15:23:28 by ncasteln          #+#    #+#              #
-#    Updated: 2023/06/14 10:16:31 by ncasteln         ###   ########.fr        #
+#    Updated: 2023/06/15 09:23:08 by ncasteln         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 CFLAGS = -Wall -Wextra -Werror
+
 SRC = push_swap.c \
 	ft_atoi.c \
 	ft_split.c \
-	lists.c
+	clst.c
 OBJS = $(SRC:.c=.o)
+
+CLST = clst.c \
+	clst_test.c \
+	ft_atoi.c \
+	ft_split.c
+CLSTOBJS = $(CLST:.c=.o)
 
 all: $(NAME)
 
@@ -30,5 +37,12 @@ fclean: clean
 	rm -f $(NAME)
 
 re: fclean all
+
+# CLISTS TESTING
+clst: $(CLSTOBJS)
+	cc $(CFLAGS) $(CLSTOBJS) -o $@
+
+clst_clean:
+	rm -f $(CLSTOBJS) clst
 
 .PHONY: all clean fclean re
--- push_swap.c
+++ push_swap.c
@@ -5,98 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/27 15:33:42 by ncasteln          #+#    #+#             */
-/*   Updated: 2023/06/15 08:37:15 by ncasteln         ###   ########.fr       */
+/*   Created: 2023/06/15 09:21:34 by ncasteln          #+#    #+#             */
+/*   Updated: 2023/06/15 09:22:07 by ncasteln         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./push_swap.h"
 
-int	main(int argc, char **argv)
+int main ()
 {
-	t_ps	*stack;
-	int i;
-
-	if (argc == 1)
-		return (1);
-	stack = malloc (sizeof(t_ps));
-	if (!stack)
-		return (1);
-	stack->a = NULL;
-	stack->b = NULL;
-	stack->max_size = argc - 1;
-
-	i = 1;
-	while (i < argc)
-	{
-		clst_append(&stack->a, clst_newnode(ft_atoi(argv[i])));
-		i++;
-	}
-	printf("[ A ] ");
-	print_clst(stack->a);
-	printf("[ B ] ");
-	print_clst(stack->b);
-	printf("\n");
-
-	// printf("__ SIZE __ \n");
-	// clst_size(stack->a);
-	// clst_size(stack->b);
-
-	// printf("__ LAST __ \n");
-	// t_clist *last = clst_last(a);
-	// printf("address outside of lst_last [%p]\n", last);
-
-	// printf("__ PUSH __ \n");
-	// clst_push(&stack->a, clst_newnode(99999));
-	// clst_push(&stack->b, clst_newnode(99999));
-
-	// printf("__ POP __\n");
-	// clst_pop(&stack->a);
-	// print_clst(stack->a);
-	// printf("\n");
-
-	printf("\n__ POP & PUSH __\n");
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	clst_push(&stack->b, clst_pop(&stack->a));
-	printf("[ A ] ");
-	print_clst(stack->a);
-	printf("[ B ] ");
-	print_clst(stack->b);
-
-	// printf("__ ROTATE (shift up) __\n");
-	// clst_rotate(&stack->a);
-	// print_clst(stack->a);
-	// clst_rotate(&stack->a);
-	// print_clst(stack->a);
-	// clst_rotate(&stack->a);
-	// print_clst(stack->a);
-	// printf("\n");
-
-	// printf("__ REV_ROTATE (shift down) __\n");
-	// clst_rev_rotate(&stack->a);
-	// print_clst(stack->a);
-	// clst_rev_rotate(&stack->a);
-	// print_clst(stack->a);
-	// clst_rev_rotate(&stack->a);
-	// print_clst(stack->a);
-	// printf("\n");
-
-	// printf("__ SWAP __\n");
-	// clst_swap(&stack->a);
-	// printf("After swap, the list is now: ");
-	// print_clst(stack->a);
-	// clst_swap(&stack->a);
-	// printf("After swap, the list is now: ");
-	// print_clst(stack->a);
-	// clst_swap(&stack->a);
-	// printf("After swap, the list is now: ");
-	// print_clst(stack->a);
-	// printf("\n");
+	return (0);
 }
