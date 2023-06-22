e3e08b63ae91e54ec4267313aee307f4ee3c1dbd
Nicolo Castelnuovo <ncasteln@3-B-3.42heilbronn.de>
2023-06-22 13:50:43 +0200

    Sorted100 but need indexing

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:14:55 by ncasteln          #+#    #+#             */
-/*   Updated: 2023/06/22 12:07:19 by ncasteln         ###   ########.fr       */
+/*   Updated: 2023/06/22 13:50:21 by ncasteln         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	sort_two(t_clist **a)
 		clst_swap(a, 'a');
 }
 
-void	sort_three(t_clist **a, int size) //  remove size ???
+void	sort_three(t_clist **a)
 {
 	if (get_biggest_node(a)->n == (*a)->n)
 	{
-		if (is_rev_sorted(*a, size))
+		if (is_rev_sorted(*a, 3))
 		{
 			clst_swap(a, 'a');
 			clst_rev_rotate(a, 'a');
@@ -36,81 +36,57 @@ void	sort_three(t_clist **a, int size) //  remove size ???
 		clst_swap(a, 'a');
 }
 
-void	sort_five(t_clist **a, t_clist **b, int size) // maybe optimize push first the 2nd biggest && remove size
+void	sort_five(t_clist **a, t_clist **b)
 {
 	if (clst_size(*a) > 4)
 	{
-		move_to_top(get_smallest_node(a), a, 'a');
+		move_to_top_best_rotation(get_smallest_node(a), a, 'a');
 		clst_pop_push(a, b, 'b');
 	}
-	move_to_top(get_biggest_node(a), a, 'a');
+	move_to_top_best_rotation(get_biggest_node(a), a, 'a');
 	clst_pop_push(a, b, 'b');
-	sort_three(a, size);
+	sort_three(a);
 	clst_pop_push(b, a, 'a');
 	clst_rotate(a, 'a');
 	clst_pop_push(b, a, 'a');
 }
 
-void	sort_hundred(t_clist **a, t_clist **b)
+void	sort_six_and_more(t_clist **a, t_clist **b)
 {
-	int		i;
-	int		n_times;
-	int		r;
-	t_clist	*head_a;
+	int	i;
+	int	j;
+	int	n_times;
+	int	size;
 
-	r = 0;
+
 	i = 0;
-	head_a = *a;
+	size = clst_size(*a);
 	n_times = highest_set(get_biggest_node(a)->n);
 	while (i <= n_times)
 	{
-		// ft_putstr_fd("\n[ New Iter ]\n\n", 1);
-		while (head_a->next != *a)
+		j = 0;
+		while (j < size)
 		{
-			if (!(is_set(head_a->n, i)))
-			{
-				r = move_to_top(head_a, a, 'a');
+			if (!is_set((*a)->n, i))
 				clst_pop_push(a, b, 'b');
-				while (r > 0)
-				{
-					clst_rev_rotate(a, 'a');
-					r--;
-				}
-				head_a = *a;
-			}
 			else
-				head_a = head_a->next;
+				clst_rotate(a, 'a');
+			j++;
 		}
-		if (!(is_set(head_a->n, i)))
-		{
-			r = move_to_top(head_a, a, 'a');
-			clst_pop_push(a, b, 'b');
-			while (r > 0)
-			{
-				clst_rev_rotate(a, 'a');
-				r--;
-			}
-		}
-		while (clst_size(*b))
+		while (*b)
 			clst_pop_push(b, a, 'a');
-		head_a = *a;
-		// ft_putstr_fd("\n[ b is empty ]\n\n", 1);
-		// clst_print(*a, 'A');
-		// clst_print(*b, 'B');
-
 		i++;
 	}
 }
 
-void	find_best_sort(t_clist **a, t_clist **b, int size)
+void	find_best_sort(t_clist **a, t_clist **b, int n_args)
 {
-	if (size == 2)
+	if (n_args == 2)
 		sort_two(a);
-	if (size == 3)
-		sort_three(a, size);
-	if (size == 4 || size == 5)
-		sort_five(a, b, size);
-	if (size > 5)
-		sort_hundred(a, b);
+	if (n_args == 3)
+		sort_three(a);
+	if (n_args == 4 || n_args == 5)
+		sort_five(a, b);
+	if (n_args > 5)
+		sort_six_and_more(a, b);
 }
-
